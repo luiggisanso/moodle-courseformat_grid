4c7cf0b1a092637081728beb4257c986941941b4
Luiggi Sansonetti <luiggi.sansonetti@univ-paris3.fr>
2012-11-15 14:44:46 +0100

    Update lang/fr/format_grid.php
    hi
    
    updated french language file
    and for information, i add the french translationinto AMOS too (but it's different ?!).
    
    and always the french documentation :
    http://docs.moodle.org/2x/fr/Grid_Format
    
    cheers

--- lang/fr/format_grid.php
+++ lang/fr/format_grid.php
@@ -1,23 +1,21 @@
 <?php
 
-$string['namegrid']='Vue en image'; // Name of a section within your format
-$string['formatgrid']='Vue en image'; // Name to display for format
-
-$string['sectionname'] = 'Section';
-$string['pluginname'] = 'Vue en image';
-
-$string['hide_summary'] = 'mettre la section en mode image';
 $string['display_summary'] = 'sortir du mode image';
-$string['hide_summary_alt'] = 'Mettre cette section en mode image';
 $string['display_summary_alt'] = 'Sortir cette section du mode image';
-
 $string['editimage'] = 'Modifier';
-$string['title'] = 'Titre de la section';
+$string['editimage_alt'] = 'Définir ou changer l\'image';
+$string['formatgrid']='Vue en image'; // Name to display for format
+$string['general_information'] = 'Information générale';
+$string['hidden_topic'] = 'Cette section a été masquée';
+$string['hide_summary'] = 'mettre la section en mode image';
+$string['hide_summary_alt'] = 'Mettre cette section en mode image';
 $string['image'] = 'Image de la section';
-$string['titlealreadyexist'] = 'le titre $a existe déjà. Merci de le modifier.';
-$string['updatesection'] = 'Modifier la section';
+$string['namegrid']='Vue en image'; // Name of a section within your format
+$string['pluginname'] = 'Vue en image';
+$string['title'] = 'Titre de la section';
+$string['topic'] = 'Section';
+$string['topic0'] = 'Général';
 $string['topicoutline'] = 'Section';
-
-$string['section0name'] = 'Général';
-
+$string['hidefromothers'] = 'Cacher la sectionc';
+$string['showfromothers'] = 'Montrer la section';
 ?>
